195fb7d99d752df95c29e96a637158fbdb7632dc
Ellesdy <VitoReiter@gmail.com>
2023-05-18 12:35:07 -0400

    chladni page

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), react(), svelte()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  }
+});
